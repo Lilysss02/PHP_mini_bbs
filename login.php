fade9eeccc8541fba821915ab9d434bc6049b41f
Lilysss02 <shkrkmtd219@gmail.com>
2019-06-19 15:14:17 +0900

    Cookieへのログイン情報保存処理

--- login.php
+++ login.php
@@ -2,7 +2,13 @@
 session_start();
 require('dbconnect.php');
 
+if ($_COOKIE['email'] !== '') {
+  $email = $_COOKIE['email'];
+}
+
 if (!empty($_POST)) {
+  $email = $_POST['email'];
+
   if ($_POST['email'] !== '' && $_POST['password'] !== '') {
     $login = $db->prepare('SELECT * FROM members WHERE email=? AND password=?');
     $login->execute(array(
@@ -15,6 +21,11 @@ if (!empty($_POST)) {
     if ($member) {
       $_SESSION['id'] = $member['id'];
       $_SESSION['time'] = time();
+
+      // Cookieにメールアドレスを保存
+      if ($_POST['save'] === 'on') {
+        setcookie('email', $_POST['email'], time()+60*60*24*14);
+      }
       header('Location: index.php');
       exit();
     } else {
@@ -49,7 +60,7 @@ if (!empty($_POST)) {
       <dl>
         <dt>メールアドレス</dt>
         <dd>
-          <input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['email'], ENT_QUOTES)); ?>" />
+          <input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($email, ENT_QUOTES)); ?>" />
           <?php if ($error['login'] === 'blank'): ?>
             <p class="error">* メールアドレスとパスワードをご記入ください</p>
           <?php endif; ?>
